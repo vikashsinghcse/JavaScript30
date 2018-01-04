3c7a12542fc4f5432f7631b49c044e3e0e33b880
Vikash Singh <vikashsinghcse@gmail.com>
2018-01-04 22:55:39 +0530

    Completed

--- drumkit/script.js
+++ drumkit/script.js
@@ -1,9 +1,22 @@
-window.addEventListener("keypress", playDrum);
+window.addEventListener("keydown", playDrum);
 
 function playDrum(e) {
   const keyNote = e.keyCode;
-  // console.log(keyNote);
   const notes = document.querySelector(`audio[data-key="${keyNote}"]`);
+  const drum = document.querySelector(`.drum[data-key="${keyNote}"]`);
+  if (!notes) {
+    return;
+  }
+  notes.currentTime = 0;
   notes.play();
-  // console.log(notes);
+  drum.classList.add("drum-playing");
 }
+
+function removePlaying(e) {
+  if (e.propertyName != "transform") {
+    return;
+  }
+  this.classList.remove("drum-playing");
+}
+const drums = document.querySelectorAll(".drum");
+drums.forEach(drum => drum.addEventListener('transitionend', removePlaying));
