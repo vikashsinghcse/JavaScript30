01ee92fdf5fbb81789f083532fca49acefc8332e
Vikash Singh <vikashsinghcse@gmail.com>
2018-01-11 14:21:05 +0530

    index.html file for flex-panels

--- drumkit/script.js
+++ drumkit/script.js
@@ -20,3 +20,12 @@ function removePlaying(e) {
 }
 const drums = document.querySelectorAll(".drum");
 drums.forEach(drum => drum.addEventListener('transitionend', removePlaying));
+window.addEventListener("keypress", playDrum);
+
+function playDrum(e) {
+  const keyNote = e.keyCode;
+  // console.log(keyNote);
+  const notes = document.querySelector(`audio[data-key="${keyNote}"]`);
+  notes.play();
+  // console.log(notes);
+}
